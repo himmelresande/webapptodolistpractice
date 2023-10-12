5c8ff9f3a6f6d346dbb317e53a3db12b1d0db498
himmelresande <119213654+himmelresande@users.noreply.github.com>
2023-10-12 15:25:40 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,34 +1,51 @@
 // src/App.js
+// Импортируем необходимые зависимости из библиотеки React.
 import React, { useState } from 'react';
-import TodoItem from './TodoItem';
-import './App.css';
 
+// Импортируем компонент TodoItem из локального файла './TodoItem'.
+import TodoItem from './TodoItem';
+
+// Импортируем стили из файла './styles.css'.
+import './styles.css';
+
+// Экспортируем компонент App, который представляет основное приложение.
 export default function App() {
+    // Используем React Hook useState для создания состояния 'todos' (список задач) и функции 'setTodos' для его обновления.
     const [todos, setTodos] = useState([]);
 
+    // Функция 'addTodo' добавляет новую задачу в список.
     const addTodo = (text) => {
+        // Проверяем, что введенный текст не пустой.
         if (text.trim() !== '') {
+            // Создаем новый массив задач, добавляя в него новую задачу с текстом и флагом завершенности (completed: false).
             setTodos([...todos, { text, completed: false }]);
         }
     };
 
+    // Функция 'deleteTodo' удаляет задачу по ее индексу в массиве.
     const deleteTodo = (index) => {
+        // Создаем новый массив задач, исключая задачу с указанным индексом.
         const newTodos = todos.filter((_, i) => i !== index);
         setTodos(newTodos);
     };
 
+    // Функция 'toggleTodo' переключает состояние задачи (завершена/не завершена) по индексу.
     const toggleTodo = (index) => {
+        // Создаем копию текущего массива задач и инвертируем состояние задачи с указанным индексом.
         const updatedTodos = [...todos];
         updatedTodos[index].completed = !updatedTodos[index].completed;
         setTodos(updatedTodos);
     };
 
+    // Функция 'updateTodoText' обновляет текст задачи по индексу.
     const updateTodoText = (index, newText) => {
+        // Создаем копию текущего массива задач и обновляем текст задачи с указанным индексом.
         const updatedTodos = [...todos];
         updatedTodos[index].text = newText;
         setTodos(updatedTodos);
     };
 
+    // Возвращаем JSX, представляющий интерфейс приложения.
     return (
         <div className="container">
             <h1>Todo List</h1>
@@ -38,6 +55,7 @@ export default function App() {
                     placeholder="Add a new task"
                     onKeyDown={(e) => {
                         if (e.key === 'Enter') {
+                            // Вызываем функцию 'addTodo' при нажатии клавиши "Enter" и сбрасываем значение ввода.
                             addTodo(e.target.value);
                             e.target.value = '';
                         }
@@ -46,6 +64,7 @@ export default function App() {
             </div>
             <ul>
                 {todos.map((todo, index) => (
+                    // Рендерим компонент TodoItem для каждой задачи, передавая ей соответствующие свойства.
                     <TodoItem
                         key={index}
                         todo={todo}
