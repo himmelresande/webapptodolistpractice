d4f01ac1e1ce0da59e051e29c6c9feb5b5597556
himmelresande <119213654+himmelresande@users.noreply.github.com>
2023-10-12 15:27:08 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Импортируем библиотеку React из установленного модуля 'react'.
+import React from 'react';
 
+// Импортируем функцию 'render' из ReactDOM, но используем расширенный путь 'react-dom/client'.
+import ReactDOM from 'react-dom/client';
+
+// Импортируем основной компонент приложения 'App' из локального файла 'App.jsx'.
+import App from './App.jsx';
+
+// Импортируем стили из файла 'index.css'.
+import './index.css';
+
+// Создаем корень (root) для рендеринга приложения в элемент с идентификатором 'root' на веб-странице.
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Оборачиваем компонент 'App' в 'React.StrictMode'. 'React.StrictMode' используется для выявления потенциальных проблем в приложении.
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
